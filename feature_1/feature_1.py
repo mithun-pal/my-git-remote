233eaf4a9f6387cd9bb9f7e4ac0ebb9d8839b463
Mithun Pal <mithunpal989@gmail.com>
2022-05-14 14:10:48 +0530

    added config read functionality

--- feature_1/feature_1.py
+++ feature_1/feature_1.py
@@ -1,1 +1,12 @@
-print("started coding for feature 1")
+import os
+import sys
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+from util.getconf import read_conf
+
+
+if __name__ == "__main__": 
+    cnfg = read_conf()
+    print("value of key1: ", cnfg["feature1"]["key1"])
--- util/getconf.py
+++ util/getconf.py
@@ -1,5 +1,15 @@
 import os
+import configparser
 
 
-script_path = os.path.abspath(__file__)
-script_root_dir = "/".join(script_path.split("/")[0:-2])
+def read_conf():
+    script_path = os.path.abspath(__file__)
+    script_root_dir = "\\".join(script_path.split("\\")[0:-2])
+    config = configparser.ConfigParser()
+    config.read(f"{script_root_dir}/conf/myapp.cnf")
+    return config
+    
+    
+if __name__ == "__main__":
+    read_conf()
+    
